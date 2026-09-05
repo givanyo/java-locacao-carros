3e5ce272b34272a66aa4bef2be87c6a7bf7b256f
givanyo <giovanniviana22@gmail.com>
2026-09-05 17:47:01 -0300

    model de informacoes da reserva

---
+++ src/model/InfoReserva.java
@@ -0,0 +1,63 @@
+package model;
+
+import java.time.LocalDate;
+
+public class InfoReserva {
+	private String grupoCarro;
+	private String modeloCarro;
+	private int duracaoDias;
+	private float valorSinal;
+	private float restante;
+	private float valorTotal;
+	private LocalDate dataPagSinal;
+	private LocalDate dataPagRestante;
+	
+	public String getGrupoCarro() {
+		return grupoCarro;
+	}
+	public void setGrupoCarro(String grupoCarro) {
+		this.grupoCarro = grupoCarro;
+	}
+	public String getModeloCarro() {
+		return modeloCarro;
+	}
+	public void setModeloCarro(String modeloCarro) {
+		this.modeloCarro = modeloCarro;
+	}
+	public int getDuracaoDias() {
+		return duracaoDias;
+	}
+	public void setDuracaoDias(int duracaoDias) {
+		this.duracaoDias = duracaoDias;
+	}
+	public float getValorSinal() {
+		return valorSinal;
+	}
+	public void setValorSinal(float valorSinal) {
+		this.valorSinal = valorSinal;
+	}
+	public float getRestante() {
+		return restante;
+	}
+	public void setRestante(float restante) {
+		this.restante = restante;
+	}
+	public float getValorTotal() {
+		return valorTotal;
+	}
+	public void setValorTotal(float valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+	public LocalDate getDataPagSinal() {
+		return dataPagSinal;
+	}
+	public void setDataPagSinal(LocalDate dataPagSinal) {
+		this.dataPagSinal = dataPagSinal;
+	}
+	public LocalDate getDataPagRestante() {
+		return dataPagRestante;
+	}
+	public void setDataPagRestante(LocalDate dataPagRestante) {
+		this.dataPagRestante = dataPagRestante;
+	}
+}
