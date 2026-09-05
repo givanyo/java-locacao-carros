1313625473ef830ad45aa9f413b27f0c6caa8cfc
givanyo <giovanniviana22@gmail.com>
2026-09-05 18:23:14 -0300

    feat: DAO de Reservas

---
+++ src/dao/ReservasDAO.java
@@ -0,0 +1,71 @@
+package dao;
+import model.InfoReserva;
+import model.Usuario;
+import java.util.ArrayList;
+import java.util.List;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import util.Conexao;
+
+public class ReservasDAO {
+	private Usuario cliente;
+	public List<InfoReserva> ConsultarReservas() {
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		String sql = "CALL selecionar_info_reservas(?)";
+        try {
+        	conn = Conexao.conectar();
+        	stmt = conn.prepareStatement(sql);
+        	stmt.setInt(1, cliente.getIdUsuario());
+        	rs = stmt.executeQuery();
+        	List<InfoReserva>reservasBanco = consultarInfoReservas(stmt, rs);
+        	return reservasBanco;
+        } catch(Exception e) {
+        	throw new RuntimeException("Erro ao consultar usuários: " + e.getMessage());
+        } finally { 
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+                if (stmt != null) {
+                    stmt.close();
+                }
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+	}
+	
+	private List<InfoReserva> consultarInfoReservas(PreparedStatement stmt, ResultSet rs) {
+		List<InfoReserva> reservasBanco = new ArrayList<>();
+		try {			
+			while(rs.next()) {
+				InfoReserva infoReserva = new InfoReserva();
+				
+				infoReserva.setGrupoCarro(rs.getString("grupo"));
+				infoReserva.setModeloCarro(rs.getString("modelo"));
+				
+				infoReserva.setDuracaoDias(rs.getInt("duracao_dias"));
+				
+				infoReserva.setValorSinal(rs.getFloat("sinal"));
+				infoReserva.setRestante(rs.getFloat("valor_restante"));
+				infoReserva.setValorTotal(rs.getFloat("valor_total"));
+				
+				infoReserva.setDataPagSinal(rs.getDate("data_pag_sinal").toLocalDate());
+				infoReserva.setDataPagRestante(rs.getDate("data_pag_restante").toLocalDate());
+				
+				reservasBanco.add(infoReserva);
+			} 
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return reservasBanco;
+	}
+}
