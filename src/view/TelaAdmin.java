a07f4e2b3c8b3068acbf2d21629aef2580c08048
givanyo <giovanniviana22@gmail.com>
2026-09-09 18:39:21 -0300

    feat: roteamento para a tela admin

---
+++ src/view/TelaAdmin.java
@@ -0,0 +1,34 @@
+package view;
+
+import java.awt.BorderLayout;
+
+import javax.swing.*;
+
+import model.Usuario;
+
+public class TelaAdmin extends JPanel{
+	private static final long serialVersionUID = 1L;
+
+	private Usuario usuario;
+	private JTabbedPane abasAdmin;
+	
+	public TelaAdmin(Usuario usuario) {
+		this.usuario = usuario;
+		setLayout(new BorderLayout(0, 0));
+
+		criarComponentes();
+		configurarEventos();
+	}
+
+	private void criarComponentes() {
+		abasAdmin = new JTabbedPane();
+		add(abasAdmin, BorderLayout.CENTER);
+
+
+	}
+
+	private void configurarEventos() {
+		abasAdmin.addChangeListener(e -> {
+		});
+	}
+}
